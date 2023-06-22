85c66090a7900fe553f4bc93dbd620e060ac1a8a
Daniel Neves Fonseca - Web Fullstack Developer <daniel.neves.fonseca.ti@gmail.com>
2023-06-21 21:25:10 -0300

    feat: first commit

---
+++ src/scripts/error.js
@@ -0,0 +1,9 @@
+function newSearch() {
+  const button = document.querySelector('.new-search');
+
+  button.addEventListener('click', () => {
+    window.location.href = '../../index.html';
+    localStorage.removeItem('user');
+  });
+}
+newSearch();
---
+++ src/scripts/index.js
@@ -0,0 +1,26 @@
+async function searchUser() {
+
+  const input = document.querySelector('.index__input');
+  const searchButton = document.querySelector('.index__button');
+
+  searchButton.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    await fetch(`https://api.github.com/users/${input.value}`, {
+      method: "GET", 
+    })
+      .then(function (response) {
+        if (response.ok) {
+          window.location.href = '../src/pages/profile.html';
+        } else {
+          window.location.href = '../src/pages/error.html';
+        }
+        return response.json();
+      })
+      .then(function (responseJson) {
+        localStorage.setItem('user', JSON.stringify(responseJson));
+      });
+  })
+}
+
+searchUser();
