00a28c90a228386a17aa28038ba3d2ba8505c070
Daniel Neves Fonseca - Web Fullstack Developer <daniel.neves.fonseca.ti@gmail.com>
2023-06-22 16:18:06 -0300

    fix: script structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,25 +2,26 @@ async function searchUser() {
 
   const input = document.querySelector('.index__input');
   const searchButton = document.querySelector('.index__button');
+  const spinner = document.querySelector('.spinner');
 
   searchButton.addEventListener('click', async (event) => {
     event.preventDefault();
 
     await fetch(`https://api.github.com/users/${input.value}`, {
-      method: "GET", 
+      method: "GET",
     })
-      .then(function (response) {
+    .then(function (response) {
+      spinner.classList.remove('hidden');
+    setTimeout(async () => {
         if (response.ok) {
-          window.location.href = './src/pages/profile.html';
+          const responseContent = await response.json();
+          localStorage.setItem('user', JSON.stringify(responseContent));
+          window.location.href = './src/pages/profile.html'; // redirecionamento de página
         } else {
           window.location.href = './src/pages/error.html';
         }
-        return response.json();
-      })
-      .then(function (responseJson) {
-        localStorage.setItem('user', JSON.stringify(responseJson));
-      });
+      }, 2000);
+    })  
   })
 }
-
 searchUser();
--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -1,61 +1,13 @@
+import { renderHeader, renderRepositories } from "./render.js";
+
 function changeUser(){
   const button = document.querySelector('.profile__header > button');
   button.addEventListener('click', () => {
     localStorage.removeItem('user');
     window.location.href = '../../index.html';
-  })
+  });
 }
 
-function renderHeader() {
-  const headerContent = document.querySelector('.profile__div');
-  const userData = JSON.parse(localStorage.getItem('user'));
-  // coloquei a limpeza dentro para renderizar caso tenha algo dentro do localStorage
-  // caso contrário têm um modelo de template renderizado
-  if (userData.length !== 0) {
-    headerContent.innerHTML = '';
-    headerContent.insertAdjacentHTML('beforebegin',
-      `
-      <div class="profile__div">
-        <img class="profile__photo" src="${userData.avatar_url}" alt="Foto de ${userData.name}" />
-        <p>${userData.name}</p>
-      </div>
-    `)
-  }
-
-}
-
-async function renderRepositories() {
-  const repositoriesContainer = document.querySelector('.profile__ul');
-  const userData = JSON.parse(localStorage.getItem('user'));
-
-  await fetch(`https://api.github.com/users/${userData.login}/repos`, {
-    method: "GET", 
-  })
-    .then(response => response.json())
-    .then(responseJson => {
-      const repositoriesArray = responseJson;
-
-      if (repositoriesArray.length !== 0) {
-          // coloquei a limpeza dentro para renderizar caso tenha algo dentro do localStorage
-          // caso contrário têm um modelo de template renderizado
-        repositoriesContainer.innerHTML = "";
-
-        repositoriesArray.forEach(repository => {
-          if (repository.description === null) {
-            repository.description = 'Repositório sem descrição';
-          }
-          repositoriesContainer.insertAdjacentHTML('afterBegin',
-            `
-          <li>
-            <h4>${repository.name}</h4>
-            <p>${repository.description}</p>
-            <a href="${repository.html_url}" target="_blank">Repositório</a>
-        </li>
-        `)
-        })
-      }
-    });
-}
 changeUser();
 renderHeader();
 renderRepositories();
